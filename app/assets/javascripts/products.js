377580e2613f70185221be5c6ee8b3ac69859a22
Mary Viets <mary.evelyn.viets@gmail.com>
2015-05-08 16:22:49 -0400

    deleted coffee files

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,12 @@
+$(document).on('ready page:load', function() {
+    $('#search-form').submit(function(event) {
+      event.preventDefault();
+      var searchValue = $('#search').val();
+
+      $.get('/products?search=' + searchValue)
+        .done(function(data) {
+          console.log(data);
+        $('#products').html(data);
+      });
+    });
+});
